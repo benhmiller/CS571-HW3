8bb8a50757f6cfc47a9bd1b6b37abc4871d45033
Benjamin Miller <benhmiller22@gmail.com>
2023-10-02 20:51:39 -0500

    Employed additional styling techniques to enhance page appeal

--- src/components/BadgerBakery.jsx
+++ src/components/BadgerBakery.jsx
@@ -5,6 +5,8 @@ import { Col, Container, Row } from "react-bootstrap";
 export default function BadgerBakery() {
 
     const [bakedGoods, setBakedGoods] = useState([]);
+    const [featuredGood, setFeaturedGood] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch("https://cs571.org/api/f23/hw3/all-baked-goods", {
@@ -19,24 +21,52 @@ export default function BadgerBakery() {
         })
     }, [])
 
+    useEffect(() => {
+        
+        fetch("https://cs571.org/api/f23/hw3/featured-baked-good", {
+            headers: {
+                "X-CS571-ID": CS571.getBadgerId()
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setFeaturedGood(data);
+            setLoading(false);
+        })
+        .catch(error => {
+            console.log("Error Fetching Data");
+            setLoading(false);
+    })
+    }, [])
+
     return <div>
-        <h1>Badger Bakery</h1>
-        <p>Welcome to our small-town bakery located in Madison, WI!</p>
         <Container>
             <Row>
-            {
-                bakedGoods.map(bakedGood => {
-                    return <Col key={bakedGood.name} xs={12} md={6} lg={4} xl={3}>
-                        <BakedGood
-                            name={bakedGood.name}
-                            description={bakedGood.description}
-                            price={bakedGood.price}
-                            featured={bakedGood.featured}
-                        />
-                    </Col>
-                })
-            }
+                <Col xs={12}>
+                    <div className="header-background-image">
+                        <div className="text-overlay">
+                            <h1>Badger Bakery</h1>
+                            <p>Welcome to our small-town bakery located in Madison, WI!</p>
+                        </div>
+                    </div>
+                </Col>
             </Row>
         </Container>
+      <Container className="baked-goods-section">
+      {loading ? <p>Loading...</p> : <p><em><strong>Today's featured item is {featuredGood.name} for ${featuredGood.price}!</strong></em></p>}
+        <Row>
+            {bakedGoods.map((bakedGood) => (
+            <Col key={bakedGood.name} xs={12} md={6} lg={4} xl={3}>
+                <BakedGood
+                name={bakedGood.name}
+                description={bakedGood.description}
+                price={bakedGood.price}
+                featured={bakedGood.featured}
+                />
+            </Col>
+            ))}
+        </Row>
+    </Container>
     </div>
 }
--- src/components/BakedGood.jsx
+++ src/components/BakedGood.jsx
@@ -1,11 +1,35 @@
+import React from 'react';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 
 export default function BakedGood(props) {
-    return <div>
-        <h2>{props.name}</h2>
-        <div>
-            <button className="inline">-</button>
-            <p className="inline">0</p>
-            <button className="inline">+</button>
+    const [numItems, setNumItems] = useState(0);
+
+    const isFeatured = props.featured ? 'featured' : '';
+
+    return (
+        <div className={`BakedGood ${isFeatured}`}>
+            <h2>{props.name}</h2>
+            <p>{props.description}</p>
+            <p>${props.price}</p>
+            <div>
+                <Button
+                    variant="primary"
+                    className={`inline ${numItems <= 0 ? 'disabled-button' : ''}`}
+                    onClick={() => setNumItems(oldNumItems => oldNumItems - 1)}
+                    disabled={numItems <= 0}
+                >
+                    -
+                </Button>
+                <p className="inline">{numItems}</p>
+                <Button
+                    variant="primary"
+                    className={`inline`}
+                    onClick={() => setNumItems(oldNumItems => oldNumItems + 1)}
+                >
+                    +
+                </Button>
+            </div>
         </div>
-    </div>
+    );
 }
